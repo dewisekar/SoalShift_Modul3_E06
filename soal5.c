194778c996fa93eb21ed8dbb96229621d6202cf5
dewisekar <sekar.6499@gmail.com>
2017-10-24 23:29:43 +0700

    sek nambah dikit mainnya

--- soal5.c
+++ soal5.c
@@ -5,13 +5,41 @@
 #include<unistd.h>
 
 File *finput;
-pthread_t tid1;
-pthread_t tid2;
+pthread_t tid[2]
+
 char str1[10],str2[10];
+int jumstr1=0, jumstr2=0;
+int status;
+int panjang;
+
+void *readandcount (void *arg){
+
+	if(status==0)
+	{
+		panjang=strlen(str1);
+
+	}
+
+}
 
 int main(){
 
 	char str1[10],str2[10];
 	scanf("%s %s", &str1, &str2);
+	int i=0;
+    int err;
+    while(i<2)//looping membuat thread 2x
+    {
+        err=pthread_create(&(tid[i]),NULL,&playandcount,NULL);//membuat thread
+        if(err!=0)//cek error
+        {
+            printf("\n can't create thread : [%s]",strerror(err));
+        }
+        else
+        {
+            printf("\n create thread success");
+        }
+        i++;
+    }
 
 }
