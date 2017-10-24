7f5adee5412c9096352e41c713401ddcbfd8eed3
dewisekar <sekar.6499@gmail.com>
2017-10-24 23:19:16 +0700

    ini baru int main aja kok hehe maksudnya tadi ini

---
+++ soal5.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<string.h>
+#include<pthread.h>
+#include<stdlib.h>
+#include<unistd.h>
+
+File *finput;
+pthread_t tid1;
+pthread_t tid2;
+char str1[10],str2[10];
+
+int main(){
+
+	char str1[10],str2[10];
+	scanf("%s %s", &str1, &str2);
+
+}
