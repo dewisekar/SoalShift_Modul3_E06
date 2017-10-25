265d1b0b448e30eff69942443152796c5d91194d
dewisekar <sekar.6499@gmail.com>
2017-10-25 11:01:23 +0700

    ini soal no 4 hehe

---
+++ soal4.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<string.h>
+#include<pthread.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<stdlib.h>
+
+int main(int argc, char** argv)
+{
+	
+}
--- soal5.c
+++ soal5.c
@@ -7,27 +7,41 @@
 FILE * finput;
 pthread_t tid[1000];
 
-int status;
+int panjang;
+char kata[100];
 
 
-
-/*void *readandcount (void *arg){
+void *readandcount (void *arg){
 	int jumlah=0;
 	finput = fopen("home/dewisekar/TUGAS_SISOP/modul3/SoalShift_Modul3_E06/Novel.txt", "r");
+    char storage[panjang+1];
 	if (finput==NULL) printf("File tidak ada!\n");
-	while(fgets(storage,strlen(argv[i]),finput)!=NULL)
+	while(fgets(storage,panjang+1,finput)!=NULL)
 	{
-		if(strcmp(argv[i],storage)==0)
+        printf("%s\n", kata);
+		if(strcmp(kata,storage)==0)
 			jumlah++;
 	}
-	//printf("%s: %d\n", argv[i],jumlah);
-
-*/
-int main(int argc, char** argv) {
-    for (int i = 0; i < argc; i++) {
-        printf("%s\n", argv[i]);
-        int panjang=strlen(argv[i]);
-        printf("%d\n", panjang);
-    }
+	printf("%s: %d\n", kata,jumlah);
+    fclose(finput);
 }
- //  fclose(finput);
+
+
+int main(int argc, char** argv) {
+    int err;
+    for (int i = 0; i < argc-1; i++) {
+        printf("%s\n", argv[i+1]);
+        panjang=strlen(argv[i+1]);
+        printf("%d\n", panjang);
+        for(int j=0; j<panjang; j++)
+            kata[j]=argv[i][j];
+        //printf("%s\n", kata);
+        err=pthread_create(&(tid[i]),NULL,&readandcount,NULL);//membuat thread
+        if(err!=0)//cek error
+        {
+            printf("\n can't create thread : [%s]",strerror(err));
+        }
+    }
+    
+}
+ 
