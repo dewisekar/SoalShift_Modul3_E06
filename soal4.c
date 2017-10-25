e3c65167cb2640edbf615e7ab08053e1f8252acf
dewisekar <sekar.6499@gmail.com>
2017-10-25 11:37:35 +0700

    tolong ini gajalan threadnya pls

--- soal4.c
+++ soal4.c
@@ -5,7 +5,34 @@
 #include<unistd.h>
 #include<stdlib.h>
 
+int in;
+pthread_t tid[100];
+
+void* faktorial(void *arg)
+{
+    int hasil=1;
+    for (int i=in; i>=1; i--)
+    	hasil=hasil*1;
+    printf("Faktorial %d: %d\n", in, hasil);
+    return NULL;
+    
+}
+
+
+
 int main(int argc, char** argv)
 {
+	
+	for(int i=0; i<argc-1; i++)
+	{
+		in=0;
+		for(int j=0; j<strlen(argv[i+1]); j++)
+		{
+			in=in*10;
+			in+=(argv[i+1][j]-'0');
+		}
+		printf("%d\n", in);
+		pthread_create(&(tid[i]), NULL, &faktorial, NULL);
+	}
 	
 }
